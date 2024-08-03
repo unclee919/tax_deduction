e0c635b127a66a60f1e617b09f2f8e3e78cf124b
frappe <frappe@demo15.pve.future-support.online>
2024-08-04 02:38:58 +0300

    Initial commit of custom ERPNext app

---
+++ hid/decoration_sales_workflow/doctype/project_stage/project_stage.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, unclee919@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Project Stage", {
+// 	refresh(frm) {
+
+// 	},
+// });
